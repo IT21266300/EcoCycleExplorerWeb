28648d7572ede4072e2a2b2b77f480de930e9920
Vihinsa Thejan Bandara <88578813+IT21266300@users.noreply.github.com>
2024-12-02 17:15:51 +0530

    add backend of admin panel web application

---
+++ w_App/backend/routes/routes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+// Routes
+// import authController from './auth/authRoutes.js';
+// import userController from './user/userRoutes.js';
+
+// Middleware Validation
+// import validateToken from '../middleware/validateTokenHandler.js';
+
+const app = express.Router();
+
+// app.use('/auth', authController);
+// app.use('/user', validateToken, userController);
+
+export default app;
