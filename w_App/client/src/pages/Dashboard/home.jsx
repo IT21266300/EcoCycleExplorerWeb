7a65a80d04bbdaa67a32e035f339c99a8a78a4aa
Kavindu Kaveesha <140581819+kavindukaveesha@users.noreply.github.com>
2024-12-03 02:52:48 +0530

    Home page card row demo finish
    I have created home page card component in component folder,card row in home folder in pages and modified home.jsx

--- w_App/client/src/pages/Dashboard/home.jsx
+++ w_App/client/src/pages/Dashboard/home.jsx
@@ -1,9 +1,42 @@
 /* eslint-disable no-unused-vars */
-import { Typography } from '@mui/material'
-import React from 'react'
+import React from "react";
+import { Typography, Box } from "@mui/material";
+import CardRow from "./Cardrow";
 
 export default function Home() {
   return (
-    <Typography variant='h1'>Home Page</Typography>
-  )
+    <Box
+      sx={{
+        width: "92vw",
+        minHeight: "100vh",
+        backgroundColor: "#f4f4f4",
+        padding: "16px",
+      }}
+    >
+      {/* Welcome Message */}
+      <Box sx={{ marginBottom: "24px", textAlign: "start",marginLeft:"10px" }}>
+        <Typography
+          variant="h4"
+          sx={{
+            fontWeight: "bold",
+            marginBottom: "8px",
+            color: "#333",
+          }}
+        >
+          Welcome to Your Dashboard
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{
+            color: "#666",
+          }}
+        >
+          Here's an overview of your key metrics. Stay updated with your data!
+        </Typography>
+      </Box>
+
+      {/* Card Row */}
+      <CardRow />
+    </Box>
+  );
 }
